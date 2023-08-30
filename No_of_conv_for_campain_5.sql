1da297ffc540a8bab6a7c5a4da94cdfab5e93a96
TharunKouda <114703721+TharunKouda@users.noreply.github.com>
2023-08-29 20:55:08 -0400

    Delete No_of_conv_for_campain_5.sql

--- No_of_conv_for_campain_5.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    md.geo AS State,
-    SUM(md.conversions) AS TotalConversions
-FROM marketing_data md
-JOIN campaign_info ci ON md.campaign_id = ci.id
-WHERE ci.name = 'Campaign5'
-GROUP BY md.geo
-ORDER BY TotalConversions DESC;
