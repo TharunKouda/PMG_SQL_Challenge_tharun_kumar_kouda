18969ee438011cb8f978a6149aafcbb6bb96303c
TharunKouda <114703721+TharunKouda@users.noreply.github.com>
2023-08-29 20:56:20 -0400

    Delete bonus_question.sql

--- bonus_question.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    DATENAME(WEEKDAY, md.date) AS DayOfWeek,
-    SUM(md.conversions) AS TotalConversions
-FROM marketing_data md
-GROUP BY DATENAME(WEEKDAY, md.date)
-ORDER BY TotalConversions DESC;
