4b7554b12070dd32a4cf9725bb0ca448e6102385
TharunKouda <114703721+TharunKouda@users.noreply.github.com>
2023-08-29 20:33:41 -0400

    Add files via upload

--- Sum_of_impressions_by_day.sql
+++ Sum_of_impressions_by_day.sql
@@ -3,3 +3,27 @@ SELECT [date],
 FROM [PMG_SQL_Task].[dbo].[marketing_data]
 GROUP BY [date]
 ORDER BY [date];
+
+
+--------------------------
+
+
+SELECT 
+     DATENAME(weekday, [date]) AS DayName,  -- Convert datetime to date to group by days
+    SUM([impressions]) AS TotalImpressions
+FROM [marketing_data]
+GROUP BY DATENAME(weekday, [date])
+ORDER BY DATENAME(weekday, [date]);
+
+
+
+
+
+
+
+
+
+
+
+
+
